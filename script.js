7c955e9234e60885471e7dd697bcaaf322912609
Romain Durand <romaindurand89@gmail.com>
2015-04-22 23:25:58 +0200

    Material floating button and free RAM monitoring
    Remember to execute npm i

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 (function() {
 	'use strict';
 	var nw = require("nw.gui");
+	var os = require('os-utils');
 	var win = nw.Window.get();
-	debugger;
+
+	initWindow();
+	setInterval(function() {
+		free_memory_span.textContent = (os.freememPercentage() * 100).toFixed(2) + "%";
+	}, 1000);
+
+	function initWindow() {
+		win.showDevTools();
+	}
 })();
