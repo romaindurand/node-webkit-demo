37dd04e051987651f1fd766fad4efe77a397e236
Romain Durand <romaindurand89@gmail.com>
2015-04-22 23:34:14 +0200

    Nice bug

--- script.js
+++ script.js
@@ -5,11 +5,26 @@
 	var win = nw.Window.get();
 
 	initWindow();
-	setInterval(function() {
-		free_memory_span.textContent = (os.freememPercentage() * 100).toFixed(2) + "%";
-	}, 1000);
+	initBindings();
 
 	function initWindow() {
 		win.showDevTools();
+		win.setTransparent(true);
+	}
+
+	function initBindings() {
+		setInterval(function() {
+			free_memory_span.textContent = (os.freememPercentage() * 100).toFixed(2) + "%";
+		}, 1000);
+
+		toggle_dev_tools.addEventListener('click', toggleDevTools);
+	}
+
+	function toggleDevTools() {
+		if (win.isDevToolsOpen()) {
+			win.closeDevTools();
+		} else {
+			win.showDevTools();
+		}
 	}
 })();
